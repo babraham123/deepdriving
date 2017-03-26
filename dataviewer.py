ee545fa2c5ee911b973a09c9f3203f5093c1c5f2
Bereket Abraham <bereketa@andrew.cmu.edu>
2017-03-26 18:31:51 -0400

    improved viewer

--- dataviewer.py
+++ dataviewer.py
@@ -5,13 +5,13 @@ import numpy as np
 
 db = plyvel.DB('../TORCS_baseline_testset/TORCS_Caltech_1F_Testing_280/')
 
-datum = caffe_pb2.Datum.FromString(db.Get('1'))
-arr = caffe.io.datum_to_array(datum)
-print(datum.label)
-print(arr.shape)
-
 print('Keys:')
 for key, value in db:
     print(key)
+
+datum = caffe_pb2.Datum.FromString(db.get(b'00000001'))
+arr = caffe.io.datum_to_array(datum)
+print(datum.label)
+print(arr.shape)
     
 db.close()
