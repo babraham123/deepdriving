3e6e6c9df2367e781ad15dbe33829171e3921aa8
Bereket Abraham <babraham42@gmail.com>
2017-03-27 04:27:16 -0400

    draft of train script

--- dataviewer.py
+++ dataviewer.py
@@ -4,6 +4,7 @@ import plyvel
 import numpy as np
 
 db = plyvel.DB('../TORCS_baseline_testset/TORCS_Caltech_1F_Testing_280/')
+# db = plyvel.DB('../TORCS_Training_1F/')
 
 print('Keys:')
 for key, value in db:
@@ -11,7 +12,25 @@ for key, value in db:
 
 datum = caffe_pb2.Datum.FromString(db.get(b'00000001'))
 arr = caffe.io.datum_to_array(datum)
-print(datum.label)
+print([i for i in datum.float_data])
 print(arr.shape)
     
 db.close()
+
+# datum = caffe.proto.caffe_pb2.Datum()
+# datum.ParseFromString(value)
+
+# datum.add_float_data(shared->angle);
+# datum.add_float_data(shared->toMarking_L);
+# datum.add_float_data(shared->toMarking_M);
+# datum.add_float_data(shared->toMarking_R);
+# datum.add_float_data(shared->dist_L);
+# datum.add_float_data(shared->dist_R);
+# datum.add_float_data(shared->toMarking_LL);
+# datum.add_float_data(shared->toMarking_ML);
+# datum.add_float_data(shared->toMarking_MR);
+# datum.add_float_data(shared->toMarking_RR);
+# datum.add_float_data(shared->dist_LL);
+# datum.add_float_data(shared->dist_MM);
+# datum.add_float_data(shared->dist_RR);
+# datum.add_float_data(shared->fast);
--- evaluate.py
+++ evaluate.py
@@ -4,17 +4,17 @@ from keras.utils import np_utils
 from alexnet import AlexNet
 import plyvel
 
-def train(db, keys, avg):
+def train(db, keys):
     m = len(keys)
     # 19 epochs
-    iterations = 140000
+    iterations = 10 # 140000
     batch_size = 64
     stream_size = batch_size * 150 # ~10K images loaded at a time
 
     model = AlexNet()
 
     for i in range(0, m, stream_size):
-        X_batch, Y_batch = get_data(db, keys[i:(i+stream_size)], avg)
+        X_train, Y_train = get_data(db, keys[i:(i+stream_size)])
         model.fit(X_batch, Y_batch, batch_size=batch_size, nb_epoch=1, verbose=1)
 
     # model.fit(X_train, Y_train,
@@ -22,60 +22,47 @@ def train(db, keys, avg):
     #       validation_data=(X_test, Y_test))
     # max_iter = #epochs * (training set/training_batch_size) 
 
+    score = model.evaluate(X_test, Y_test, batch_size=64, verbose=1)
+    print('Test score:', score)
+
     return model
 
 
 def get_data(dbpath, keys):
-    X_train = np.array([])
-    Y_train = np.array([])
-
     for key in keys:
         datum = caffe_pb2.Datum.FromString(db.get(key))
         img = caffe.io.datum_to_array(datum)
         # img.shape = 3x210x280
-        img = img.reshape(1, 3*210*280) / 255
-        img = np.subtract(img, avg);
-        X_train = np.concatenate((X_train, img))
-
         affordances = [i for i in datum.float_data]
-        affordances = M = np.array(affordances)
-        affordances = affordances.reshape(1, 14)
-        Y_train = np.concatenate((Y_train, affordances))
 
     # resize 3x210x280
     # subtract mean
     # crop = 0, mirror = false
     # shuffle
 
+    print("X_train original shape", X_train.shape)
+    print("y_train original shape", y_train.shape)
+
+    X_train = X_train.reshape(60000, 784)
+    X_test = X_test.reshape(10000, 784)
     X_train = X_train.astype('float32')
-    Y_train = Y_train.astype('float32')
-    return X_train, Y_train
+    X_test = X_test.astype('float32')
+    X_train /= 255
+    X_test /= 255
+    print("Training matrix shape", X_train.shape)
+    print("Testing matrix shape", X_test.shape)
 
-
-def calc_average(db, keys):
-    avg = np.zeros((3, 210, 280))
-    n = 0
-
-    for key in keys:
-        datum = caffe_pb2.Datum.FromString(db.get(key))
-        img = caffe.io.datum_to_array(datum)
-        
-        avg = np.add(avg*n, img) / (n+1)
-        n = n+1
-
-    avg = avg.reshape(1, 3*210*280) / 255
-    return avg
+    return {'X_train': X_train, 'Y_train': Y_train, 'X_test': X_test, 'Y_test': Y_test}
 
 
 if __name__ == "__main__":
-    dbpath = '../TORCS_Training_1F/'
+    dbpath = '../TORCS_baseline_testset/TORCS_Caltech_1F_Testing_280/'
     db = plyvel.DB(dbpath)
     keys = []
     for key, value in db:
         keys.append(key)
 
-    avg = calc_average(db, keys)
-    model = train(db, keys, avg)
+    model = train(db, keys)
 
     model.save('deepdriving_model.h5')
     model.save_weights('deepdriving_weights.h5')
