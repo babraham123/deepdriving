828651225a078d1b501e4d58e58070dea5e312f7
Bereket Abraham <babraham42@gmail.com>
2017-03-29 12:40:58 -0400

    fixed comments

--- alexnet.py
+++ alexnet.py
@@ -27,12 +27,6 @@ def AlexNet(weights_path=None):
     # in caffe: Local Response Normalization (LRN)
     # alpha = 1e-4, k=2, beta=0.75, n=5,
     conv_2 = ZeroPadding2D((2,2))(conv_2)
-
-    # conv_2 = merge([
-    #     Convolution2D(128, 5, 5, activation="relu", name='conv_2_'+str(i+1))(
-    #         splittensor(ratio_split=2, id_split=i)(conv_2)
-    #     ) for i in range(2)], mode='concat', concat_axis=1, name="conv_2")
-
     # split unnecessary on modern GPUs, no stride
     conv_2 = Convolution2D(256, 5, 5, activation="relu", name='conv_2')(conv_2)
 
@@ -42,20 +36,10 @@ def AlexNet(weights_path=None):
     conv_3 = Convolution2D(384, 3, 3, activation='relu', name='conv_3')(conv_3)
 
     conv_4 = ZeroPadding2D((1,1))(conv_3)
-    # conv_4 = merge([
-    #     Convolution2D(192,3,3,activation="relu",name='conv_4_'+str(i+1))(
-    #         splittensor(ratio_split=2,id_split=i)(conv_4)
-    #     ) for i in range(2)], mode='concat',concat_axis=1,name="conv_4")
-
     # split unnecessary on modern GPUs, no stride
     conv_4 = Convolution2D(384, 3, 3, activation="relu", name='conv_4')(conv_4)
 
-    conv_5 = ZeroPadding2D((1,1))(conv_4)
-    # conv_5 = merge([
-    #     Convolution2D(128,3,3,activation="relu",name='conv_5_'+str(i+1))(
-    #         splittensor(ratio_split=2,id_split=i)(conv_5)
-    #     ) for i in range(2)], mode='concat',concat_axis=1,name="conv_5")
-    
+    conv_5 = ZeroPadding2D((1,1))(conv_4)    
     # split unnecessary on modern GPUs, no stride
     conv_5 = Convolution2D(256, 3, 3, activation="relu", name='conv_5')(conv_5)
     dense_1 = MaxPooling2D((3, 3), strides=(2,2), name="convpool_5")(conv_5)
