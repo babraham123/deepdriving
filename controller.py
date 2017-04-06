c621760b729f00b719cca295f5753454fb5c4724
Bereket Abraham <babraham42@gmail.com>
2017-04-06 17:48:37 -0400

    drafted controller

---
+++ controller.py
@@ -0,0 +1,175 @@
+# a controller processes the cnn output and get the optimal steering, acceleration/brake
+slow_down = 100.0
+
+if (pre_dist_L < 20.0 and dist_LL < 20.0):  # left lane is occupied or not
+    left_clear = 0
+    left_timer = 0
+else:
+    left_timer = left_timer + 1
+
+if (pre_dist_R < 20.0 and dist_RR < 20.0):  # right lane is occupied or not
+    right_clear = 0
+    right_timer = 0
+else:
+    right_timer = right_timer + 1
+
+pre_dist_L = dist_LL
+pre_dist_R = dist_RR
+
+if (left_timer > timer_set):  # left lane is clear
+    left_timer = timer_set
+    left_clear = 1
+
+if (right_timer > timer_set):  # right lane is clear
+    right_timer = timer_set
+    right_clear = 1
+
+
+if (lane_change == 0 and dist_MM < 15.0):  # if current lane is occupied
+    steer_trend = steering_record[0] + steering_record[1] + steering_record[2] + steering_record[3] + steering_record[4]  # am I turning or not
+
+    if (toMarking_LL > -8.0 and left_clear == 1 and steer_trend >= 0 and steer_trend < 0.2):  # move to left lane
+        lane_change = -2
+        coe_steer = 6
+        right_clear = 0
+        right_timer = 0
+        left_clear = 0
+        left_timer = 30
+        timer_set = 60
+
+    elif (toMarking_RR < 8.0 and right_clear == 1 and steer_trend <= 0 and steer_trend > -0.2):  # move to right lane
+        lane_change = 2
+        coe_steer = 6
+        left_clear = 0
+        left_timer = 0
+        right_clear = 0
+        right_timer = 30
+        timer_set = 60
+
+    else:
+        v_max = 20.0
+        c = 2.772
+        d = -0.693
+        slow_down = v_max * (1 - exp(-c / v_max * dist_MM - d))  # optimal velocity car-following model
+        if (slow_down < 0):
+            slow_down = 0
+
+
+######### prefer to stay in the central lane
+elif (lane_change == 0 and dist_MM >= 15):
+    steer_trend = steering_record[0] + steering_record[1] + steering_record[2] + steering_record[3] + steering_record[4]  # am I turning or not
+
+    if (toMarking_RR > 8 and left_clear == 1 and steer_trend >= 0 and steer_trend < 0.2):  # in right lane, move to central lane
+        lane_change = -2
+        coe_steer = 6
+        left_clear = 0
+        left_timer = 30
+
+    elif (toMarking_LL < -8 and right_clear == 1 and steer_trend <= 0 and steer_trend > -0.2):  # in left lane, move to central lane
+        lane_change = 2
+        coe_steer = 6
+        right_clear = 0
+        right_timer = 30
+
+########## END prefer to stay in the central lane
+
+########### implement lane changing or car-following
+if (lane_change == 0):
+    if ((-toMarking_ML + toMarking_MR) < 5.5):
+        coe_steer = 1.5
+        center_line = (toMarking_ML + toMarking_MR) / 2.0
+        pre_ML = toMarking_ML
+        pre_MR = toMarking_MR
+        if (toMarking_M < 1):
+            coe_steer = 0.4
+    else:
+        if (-pre_ML > pre_MR):
+            center_line = (toMarking_L + toMarking_M) / 2.0
+        else:
+            center_line = (toMarking_R + toMarking_M) / 2.0
+        coe_steer = 0.3
+
+elif (lane_change == -2):
+    if ((-toMarking_ML + toMarking_MR) < 5.5):
+        center_line = (toMarking_LL + toMarking_ML) / 2.0
+        if (toMarking_L > -5 and toMarking_M < 1.5):
+            center_line = (center_line + (toMarking_L + toMarking_M) / 2.0) / 2.0
+    else:
+        center_line = (toMarking_L + toMarking_M) / 2.0
+        coe_steer = 20.0
+        lane_change = -1
+
+elif (lane_change == -1):
+    if (toMarking_L > -5 and toMarking_M < 1.5):
+        center_line = (toMarking_L + toMarking_M) / 2.0
+        if ((-toMarking_ML + toMarking_MR) < 5.5):
+            center_line = (center_line + (toMarking_ML + toMarking_MR) / 2.0) / 2.0
+    else:
+        center_line = (toMarking_ML + toMarking_MR) / 2.0
+        lane_change = 0
+
+elif (lane_change == 2):
+    if ((-toMarking_ML + toMarking_MR) < 5.5):
+        center_line = (toMarking_RR + toMarking_MR) / 2.0
+        if (toMarking_R < 5.0 and toMarking_M < 1.5):
+            center_line = (center_line + (toMarking_R + toMarking_M) / 2.0) / 2.0
+    else:
+        center_line = (toMarking_R + toMarking_M) / 2.0
+        coe_steer = 20.0
+        lane_change = 1
+
+elif (lane_change == 1):
+    if (toMarking_R < 5 and toMarking_M < 1.5):
+        center_line = (toMarking_R + toMarking_M) / 2.0
+        if ((-toMarking_ML + toMarking_MR) < 5.5):
+            center_line = (center_line + (toMarking_ML + toMarking_MR) / 2.0) / 2.0
+    else:
+        center_line = (toMarking_ML + toMarking_MR) / 2.0
+        lane_change = 0
+
+############## END implement lane changing or car-following
+# ??? shared->steerCmd
+steerCmd = (angle - (center_line / road_width)) / 0.541052 / coe_steer  # steering control, "steerCmd" [-1,1] is the value sent back to TORCS
+
+if (lane_change == 0 and coe_steer > 1 and steerCmd > 0.1):   # reshape the steering control curve
+    steerCmd = steerCmd * (2.5 * steerCmd + 0.75)
+
+steering_record[steering_head] = steerCmd  # update previous steering record
+steering_head = steering_head + 1
+if (steering_head == 5):
+    steering_head = 0
+
+
+if (fast == 1):
+    desired_speed = 20.0
+else:
+    ssum = steering_record[0] + steering_record[1] + steering_record[2] + steering_record[3] + steering_record[4]
+    desired_speed = 20.0 - abs(ssum) * 4.5
+if (desired_speed < 10):
+    desired_speed = 10.0
+
+if (slow_down < desired_speed):
+    desired_speed = slow_down
+
+############ speed control
+# ??? shared->speed, shared->accelCmd, shared->brakeCmd      
+if (desired_speed >= speed):
+    accelCmd = 0.2 * (desired_speed - speed + 1)
+    if (accelCmd > 1):
+        accelCmd = 1.0
+    brakeCmd = 0.0
+else:
+    brakeCmd = 0.1 * (speed - desired_speed)
+    if (brakeCmd > 1):
+        brakeCmd = 1.0
+    accelCmd = 0.0
+
+########### END speed control
+
+print("M_LL:%.2lf, M_ML:%.2lf, M_MR:%.2lf, M_RR:%.2lf, d_LL:%.2lf, d_MM:%.2lf, d_RR:%.2lf",
+    (toMarking_LL, toMarking_ML, toMarking_MR, toMarking_RR, dist_LL, dist_MM, dist_RR))
+print("M_L:%.2lf, M_M:%.2lf, M_R:%.2lf, d_L:%.2lf, d_R:%.2lf, angle:%.3lf, fast:%d",
+    (toMarking_L, toMarking_M, toMarking_R, dist_L, dist_R, angle, fast))
+print("coe_steer:%.1lf, lane_change:%d, steer:%.2lf, d_speed:%d, speed:%d, l_clear:%d, r_clear:%d, timer_set:%d\n",
+    (coe_steer, lane_change, steerCmd, floor(desired_speed * 3.6), floor(speed * 3.6), left_clear, right_clear, timer_set))
+########## END a controller processes the cnn output and get the optimal steering, acceleration/brake
--- deepdrive_agent.py
+++ deepdrive_agent.py
@@ -1,44 +1,65 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from keras import backend as K
+from keras.models import load_model
+import h5py
 
-from convnetskeras.convnets import preprocess_image_batch
-from alexnet import AlexNet
 
 class Agent(object):
     def __init__(self, dim_action):
-        self.dim_action = dim_action
-        self.model = AlexNet(weights_path="deepdriving_weights.h5")
+        self.model = load_model('deepdriving_model_rand.h5')
+        self.average = self.load_average()
+        self.is_tf = (K.image_dim_ordering() == 'tf')
+
+    def load_average():
+        h5f = h5py.File('deepdriving_average.h5', 'r')
+        avg = h5f['average'][:]
+        h5f.close()
+        return avg
+
+    def preprocess_image(self, vision):
+        im = np.resize(vision, (210, 280, 3))
+        # crop, zero pad, etc 
+        if self.is_tf is False:
+            im = np.swapaxes(im, 0, 1)
+        return im
+
+    def controller(self, affordances):
+        action = [0.0, 0.0]
+        return action
 
     def act(self, ob, reward, done, vision_on):
-        #print("ACT!")
+        '''
+        Get an Observation from the environment.
+        Each observation vectors are numpy array.
+        focus, opponents, track sensors are scaled into [0, 1]. When the agent
+        is out of the road, sensor variables return -1/200.
+        rpm, wheelSpinVel are raw values and then needed to be preprocessed.
+        vision is given as a tensor with size of (64*64, 3) = (4096, 3) <-- rgb
+        and values are in [0, 255]
+        '''
 
-        # Get an Observation from the environment.
-        # Each observation vectors are numpy array.
-        # focus, opponents, track sensors are scaled into [0, 1]. When the agent
-        # is out of the road, sensor variables return -1/200.
-        # rpm, wheelSpinVel are raw values and then needed to be preprocessed.
-        # vision is given as a tensor with size of (64*64, 3) = (4096, 3) <-- rgb
-        # and values are in [0, 255]
-
-        # im = preprocess_image_batch(['examples/dog.jpg'],img_size=(256,256), crop_size=(227,227), color_mode="rgb")
-        # out = model.predict(im)
-
-        if vision_on is False:
-            focus, speedX, speedY, speedZ, opponents, rpm, track, wheelSpinVel = ob
-        else:
+        if vision_on:
             focus, speedX, speedY, speedZ, opponents, rpm, track, wheelSpinVel, vision = ob
-
-            """ The code below is for checking the vision input. This is very heavy for real-time Control
-                So you may need to remove.
-            """
-            print(vision.shape)
-            """
+            '''
+            The code below is for checking the vision input. 
+            This is very heavy for real-time Control
+            So you may need to remove.
+            
             img = np.ndarray((64,64,3))
             for i in range(3):
                 img[:, :, i] = 255 - vision[:, i].reshape((64, 64))
             plt.imshow(img, origin='lower')
             plt.draw()
             plt.pause(0.001)
-            """
-        return np.tanh(np.random.randn(self.dim_action)) # random action
+            '''
 
+            im = self.preprocess_image(vision)
+            affordances = self.model.predict(im)
+            action = self.controller(affordances)
+
+        else:
+            focus, speedX, speedY, speedZ, opponents, rpm, track, wheelSpinVel = ob
+            action = np.tanh(np.random.randn(2))  # random action
+
+        # action = (steering angle, throttle amt)
+        return action
