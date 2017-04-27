ed16bbb309ebffa3a76af394c2f9c0f706f7218a
Bereket Abraham <babraham42@gmail.com>
2017-04-27 16:13:30 -0400

    eval script

--- train_alexnet.py
+++ train_alexnet.py
@@ -6,7 +6,7 @@ from keras import backend as K
 from keras.optimizers import Adam, SGD
 from keras.layers import Dense, Dropout
 from keras.models import Model
-from keras.callbacks import TensorBoard,CSVLogger,ModelCheckpoint,EarlyStopping,ReduceLROnPlateau
+from keras.callbacks import TensorBoard, CSVLogger, ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from convnetskeras.convnets import AlexNet
 import cv2
 from glob import glob
@@ -24,11 +24,11 @@ model_filename = '/home/lkara/deepdrive/deepdriving/models/model%d.json' % model
 weights_filename = '/home/lkara/deepdrive/deepdriving/models/model%d.h5' % model_num
 csvlog_filename = '/home/lkara/deepdrive/deepdriving/models/model%d.csv' % model_num
 
-##  tensorboard --logdir /home/lkara/deepdrive/deepdriving/models/
+#  tensorboard --logdir /home/lkara/deepdrive/deepdriving/models/
 tbCallBack = TensorBoard(log_dir=logs_path, histogram_freq=0, write_graph=True, write_images=False)
-csvlog = CSVLogger(csvlog_filename, separator=',', append=False )
+csvlog = CSVLogger(csvlog_filename, separator=',', append=False)
 mdlchkpt = ModelCheckpoint(weights_filename, monitor='val_loss', save_best_only=True, save_weights_only=True, period=2)
-erlystp = EarlyStopping(monitor='val_mean_absolute_error',min_delta=1e-4,patience=10) 
+erlystp = EarlyStopping(monitor='val_mean_absolute_error', min_delta=1e-4, patience=10)
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.8, patience=5, min_lr=1e-5)
 
 K.set_image_dim_ordering('th')
@@ -45,21 +45,21 @@ else:
 
 
 def train(db, keys, avg):
-    m = 100000 # len(keys)
+    m = 100000  # len(keys)
 
     batch_size = 16  # powers of 2
     stream_size = batch_size * 500  # 16K images loaded at a time
     epochs = 65
-    
+
     model = get_model()
 
     for i in range(0, m, stream_size):
         print(i, 'iteration')
         X_batch, Y_batch = get_data(db, keys[i:(i + stream_size)], avg)
-        model.fit(X_batch, Y_batch, 
-                  batch_size=batch_size, epochs=epochs, 
-                  validation_split=0.2, verbose=2, 
-                  callbacks=[csvlog,reduce_lr,mdlchkpt,tbCallBack])  # tbCallBack
+        model.fit(X_batch, Y_batch,
+                  batch_size=batch_size, epochs=epochs,
+                  validation_split=0.2, verbose=2,
+                  callbacks=[csvlog, reduce_lr, mdlchkpt, tbCallBack])  # tbCallBack
 
     return model
 
@@ -68,7 +68,7 @@ def get_model():
     base_model = AlexNet(weights_path='alexnet_weights.h5', dim=dim)
     for layer in base_model.layers:
         layer.trainable = False
-        
+
     x = base_model.output
     x = Dense(512, activation='relu', init='glorot_normal', name='fc1')(x)
     x = Dense(512, activation='relu', init='glorot_normal', name='fc2')(x)
@@ -79,10 +79,10 @@ def get_model():
     model = Model(input=base_model.input, output=x)
     model.summary()
 
-    adam = Adam(lr = 1e-4)
+    adam = Adam(lr=1e-4)
     model.compile(optimizer=adam, loss='mse')
-    #sgd = SGD(lr=0.05, decay=0.0005, momentum=0.9)    
-    #model.compile(optimizer=sgd, loss='mse')
+    # sgd = SGD(lr=0.05, decay=0.0005, momentum=0.9)
+    # model.compile(optimizer=sgd, loss='mse')
     return model
 
 
@@ -102,9 +102,9 @@ def get_data(db, keys, avg):
         img = img.astype('float32')
 
         # convnet preprocessing using during training
-        img[:,:,0] -= 123.68
-        img[:,:,1] -= 116.779
-        img[:,:,2] -= 103.939
+        img[:, :, 0] -= 123.68
+        img[:, :, 1] -= 116.779
+        img[:, :, 2] -= 103.939
         # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
         # img = img / 255.0
@@ -116,7 +116,7 @@ def get_data(db, keys, avg):
         X_train[i] = img
 
         j = int(key[-12:-4])
-        affordances = db[j-1]
+        affordances = db[j - 1]
         if int(affordances[0]) != j:
             raise ValueError('Image and affordance do not match: ' + str(j))
         affordances = affordances[1:]
